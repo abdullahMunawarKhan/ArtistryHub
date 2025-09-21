b27ad6557a1bde846e1222e740c1680b6e796967
abdullahMunawarKhan <abdullahk4503@gmail.com>
2025-09-21 10:36:24 +0530

    sitemaps build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import sitemap from 'vite-plugin-sitemap'
+
+const siteUrl = process.env.SITE_URL || 'https://scopebrush.vercel.app'
 
 export default defineConfig({
-  plugins: [react()]
-   
+  plugins: [
+    react()
+    ,
+    sitemap({
+      hostname: siteUrl,    // your production URL
+      outDir: 'dist',                         // where Vite builds to
+      routes: async () => {
+        ['/', '/user-login', '/artist-profile/:artistId', '/main-dashboard',
+          '/signup', 'update-password', '/frtfgau84hfdja', '/dshakfgadsj',
+          '/artist-list', 'artist-profile', 'cart', 'register', '/orders',
+          '/upload-work', 'order-process', '/,product', '/track-order',
+          'track-order/:trackingId', '/artist-dashboard', '/feed', '/feedback-form',
+          '/contact-us', '/privacy-policies', '/terms-conditions']
+
+        return []
+      }
+
+
+    })
+  ]
+
 })
